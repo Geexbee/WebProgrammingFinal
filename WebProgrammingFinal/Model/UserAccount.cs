f7e1836b8bef8c033841efcd05601c8efbe911ba
Gareth Hillard <gareth.hillard@googlemail.com>
2017-04-20 16:23:36 +0100

    Data Annotation validation

--- WebProgrammingFinal/Model/UserAccount.cs
+++ WebProgrammingFinal/Model/UserAccount.cs
@@ -1,6 +1,7 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using System;
 using System.Collections.Generic;
+using System.ComponentModel;
 using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Threading.Tasks;
@@ -11,33 +12,37 @@ namespace WebProgrammingFinal.Model
     {
         [Key]
         public int UserID { get; set; }
-
-        [Required(ErrorMessage = "First name is required.")]
+        [StringLength(55), Required(ErrorMessage = "First name is required.")]
+        [DisplayName("First Name")]
         public string FirstName { get; set; }
 
-        [Required(ErrorMessage = "Last name is required.")]
+        [StringLength(55), Required(ErrorMessage = "Last name is required.")]
+        [DisplayName("Last Name")]
         public string LastName { get; set; }
 
-        [Required(ErrorMessage = "Email address is required.")]
+        [StringLength(120), Required(ErrorMessage = "Email address is required.")]
         [DataType(DataType.EmailAddress)]
+        [DisplayName("Email")]
         public string Email { get; set; }
 
-        [Required(ErrorMessage = "Username is required.")]
-        [Remote("doesUserNameExist", "Account", HttpMethod = "POST", ErrorMessage = "User name already exists. Please enter a different user name.")]
+        [StringLength(25), Required(ErrorMessage = "Username is required.")]
+        //  Not sure if i can get this working, but got duplications validated in code anyway.
+        // [Remote("doesUserNameExist", "Account", HttpMethod = "POST", ErrorMessage = "User name already exists. Please enter a different user name.")]
+        [DisplayName("Username")]
         public string Username { get; set; }
 
         [Required(ErrorMessage = "Password is required.")]
         [DataType(DataType.Password)]
+        [DisplayName("Password")]
+        [RegularExpression("^([a-zA-Z0-9@*#]{8,25})$", ErrorMessage = "Password format invalid.")]
         public string Password { get; set; }
 
-        [Compare("Password", ErrorMessage = "Passwords do not match")]
+        [Compare("Password", ErrorMessage = "Passwords do not match.")]
         [DataType(DataType.Password)]
+        [DisplayName("Confirm Password")]
         public string ConfirmPassword { get; set; }
 
-        internal static object GetUser(string username)
-        {
-            throw new NotImplementedException();
-        }
+        
 
         // Other properties and the validation for them.
     }
